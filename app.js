f9252eb94d6bde608f6c3744c76b29dd4f773994
donniebreve <donniebreve@gmail.com>
2020-07-12 16:54:33 -0700

    added birthdate input

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 'use strict';
 
-document.addEventListener("DOMContentLoaded", function () {
-
-    let person = new Person('<YOUR BIRTHDATE>').setYears(90).save();
+function generate() {
+    let person = new Person(this.value).setYears(90).save();
     let weeksLived = person.weeks;
     let boxes = person.getBoxes();
     let grid = document.getElementById('grid');
-
+    grid.innerHTML = '';
     for (let i = 0; i < boxes; i++) {
         let div = document.createElement('div');
         div.setAttribute('class', 'box-grid');
-
         if (i < weeksLived) {
             div.setAttribute('class', 'box-grid-complete');
         }
-
         grid.appendChild(div);
     }
-    
-});
+}
+
+let input = document.getElementById('birthdate');
+input.onchange = generate;
+input.onkeydown = function(event) {
+    if (event.keyCode === 13) {
+        generate.call(input);
+    }
+};
